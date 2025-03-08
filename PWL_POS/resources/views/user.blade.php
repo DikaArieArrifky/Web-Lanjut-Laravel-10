918dc98dd07a87553785c5bd11c9943acd5f969f
DikaArieArrifky <20dika16@gmail.com>
2025-03-08 20:30:33 +0700

    Praktikum 2.3 – Retreiving Aggregrates

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -8,22 +8,12 @@
 </head>
 
 <body>
-    
+
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
-        </tr>
-        <tr>
-            <th>{{ $data->user_id }}</th>
-            <th>{{ $data->username }}</th>
-            <th>{{ $data->nama }}</th>
-            <th>{{ $data->level_id }}</th>
-        </tr>
         
+        <p>berjumlah {{ $data }}</p>
+
     </table>
 </body>
 
