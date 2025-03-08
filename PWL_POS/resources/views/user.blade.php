d56aec2390472522af55bcc1c36c9baa2bda09da
DikaArieArrifky <20dika16@gmail.com>
2025-03-09 03:08:24 +0700

    Praktikum 2.7 – Relationships

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -19,6 +19,8 @@
             <td>Username</td>
             <td>Nama</td>
             <td>ID Level Pengguna</td>
+            <td>Level Kode</td>
+            <td>Nama Level</td>
             <td>Aksi</td>
         </tr>
         @foreach ($data as $d)
@@ -27,6 +29,8 @@
             <td>{{ $d->username }}</td>
             <td>{{ $d->nama }}</td>
             <td>{{ $d->level_id }}</td>
+            <td>{{ $d->level->level_kode }}</td>
+            <td>{{ $d->level->level_nama }}</td>
             <td><a href="user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="user/hapus/{{ $d->user_id }}">Hapus"></a></td>
         </tr>
         @endforeach
