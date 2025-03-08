620b32ace26d238d9fa559c4cae0d6526b3db1a2
DikaArieArrifky <20dika16@gmail.com>
2025-03-09 02:53:00 +0700

    Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -37,6 +37,18 @@ class UserController extends Controller
         // return view('user', ['data' => $users]);
 
         //----------------------------JS 4 ------------------------------
+        //Praktikum 1 - $fillable
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
         // $data = [
         //     'level_id' => 2,
         //     'username' => 'manager_tiga',
@@ -48,25 +60,39 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
 
         //$user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
         //$user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
         //$user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
 
         // $user = UserModel::findOr(1, ['username', 'nama'], function() {
         //     abort(404);
         // });
+        // return view('user', ['data' => $user]);
 
         // $user = UserModel::findOr(20, ['username', 'nama'], function() {
         //     abort(404);
         // });
+        // return view('user', ['data' => $user]);
 
+        //PRAKTIKUM 2.2
         //$user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
 
         //$user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
 
+        //PRAKTIKUM 2.3
         // $user = UserModel::where('level_id', 2)->count();
         // //dd($user);
+        // return view('user', ['data' => $user]);
 
-        //----------------praktikum 2.4 retreiving or creating models
+        
+
+        //praktikum 2.4 retreiving or creating models
         // $user = UserModel::firstOrCreate(
         //     [
         //         'username' => 'manager',
@@ -143,24 +169,66 @@ class UserController extends Controller
         // $user->isClean();//true
         // dd($user->isDirty());
         
-        $user = UserModel::create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        // $user = UserModel::create(
+        //     [
+        //         'username' => 'manager11',
+        //         'nama' => 'Manager11',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
 
-        $user->username = 'manager12';
+        // $user->username = 'manager12';
+        // $user->save();
+
+        // $user->wasChanged();//true
+        // $user->wasChanged('username'); //false
+        // $user->wasChanged('username', 'level_id'); //true
+        // $user->wasChanged('nama'); //false
+        // dd($user->wasChanged(['nama','username']));//true
+
+        //praktikum 2.6
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+
+    }
+
+    //praktikum 2.6
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request){
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function ubah($id){
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request){
+        $user = UserModel::find($id);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
         $user->save();
 
-        $user->wasChanged();//true
-        $user->wasChanged('username'); //false
-        $user->wasChanged('username', 'level_id'); //true
-        $user->wasChanged('nama'); //false
-        dd($user->wasChanged(['nama','username']));//true
-        
-        
+        return redirect('/user');
+    }
+
+    public function hapus($id){
+        $user = UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
     }
 }
--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -9,23 +9,27 @@
 
 <body>
 
+    <!-- Praktikum 2.6 -->
+
     <h1>Data User</h1>
+    <a href="user/tambah">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Aksi</td>
         </tr>
+        @foreach ($data as $d)
         <tr>
-            <th>{{ $data->user_id }}</th>
-            <th>{{ $data->username }}</th>
-            <th>{{ $data->nama }}</th>
-            <th>{{ $data->level_id }}</th>
-
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+            <td><a href="user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="user/hapus/{{ $d->user_id }}">Hapus"></a></td>
         </tr>
-
-
+        @endforeach
     </table>
 </body>
 
