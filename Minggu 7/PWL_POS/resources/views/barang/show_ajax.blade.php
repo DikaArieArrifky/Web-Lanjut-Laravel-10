409e173bfc6dfd5b4542670d71b908d7aa58ca00
DikaArieArrifky <20dika16@gmail.com>
2025-04-03 17:31:03 +0700

    Praktikum 1 – Implementasi Authentication

---
+++ Minggu 7/PWL_POS/resources/views/barang/show_ajax.blade.php
@@ -0,0 +1,45 @@
+<div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title">Detail Barang</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <table class="table table-bordered">
+                <tr>
+                    <th>Kategori</th>
+                    <td>{{ $barang->kategori->kategori_nama }}</td>
+                </tr>
+                <tr>
+                    <th>Kode Barang</th>
+                    <td>{{ $barang->barang_kode }}</td>
+                </tr>
+                <tr>
+                    <th>Nama Barang</th>
+                    <td>{{ $barang->barang_nama }}</td>
+                </tr>
+                <tr>
+                    <th>Harga Beli</th>
+                    <td>Rp {{ number_format($barang->harga_beli, 0, ',', '.') }}</td>
+                </tr>
+                <tr>
+                    <th>Harga Jual</th>
+                    <td>Rp {{ number_format($barang->harga_jual, 0, ',', '.') }}</td>
+                </tr>
+                <tr>
+                    <th>Dibuat Pada</th>
+                    <td>{{ $barang->created_at }}</td>
+                </tr>
+                <tr>
+                    <th>Diperbarui Pada</th>
+                    <td>{{ $barang->updated_at }}</td>
+                </tr>
+            </table>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+        </div>
+    </div>
+</div>
